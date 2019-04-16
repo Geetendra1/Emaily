705d2044b36e614ac09d90519a9948c4cc24a825
Geetendra1 <43998658+Geetendra1@users.noreply.github.com>
2019-04-16 11:01:59 +0530

    done2

---
+++ server/client/src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Emaily</h1>
+      Collect feedback from your users
+    </div>
+  );
+};
+export default Landing;
